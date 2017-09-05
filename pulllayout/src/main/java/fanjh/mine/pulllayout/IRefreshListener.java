41efa69a8c477d7a2d3549e7f8f432cc3fd90f59
fanjinhao <fanjinhao@91160.com>
2017-09-05 09:20:08 +0800

    添加注释

--- pulllayout/src/main/java/fanjh/mine/pulllayout/IRefreshListener.java
+++ pulllayout/src/main/java/fanjh/mine/pulllayout/IRefreshListener.java
@@ -7,9 +7,9 @@ package fanjh.mine.pulllayout;
 * @note 可以通过实现该回调，处理各种状态
 **/
 public interface IRefreshListener {
-    void onBeforeRefresh();//没有达到刷新条件头部开始回弹的回调
-    void onRefreshBegin();
-    void onUIPositionChanged(int oldOffset, int newOffset, int refreshOffset);
-    void onRefreshComplete();
-    void onCanRefresh();
+    void onBeforeRefresh();//当前偏移量没有达到刷新的标准时松手，然后头部开始回弹的回调
+    void onRefreshBegin();//开始刷新的回调
+    void onUIPositionChanged(int oldOffset, int newOffset, int refreshOffset);//视图滑动过程中的回调
+    void onRefreshComplete();//刷新完成的回调
+    void onCanRefresh();//当前偏移量已经超过刷新的标准的时候，还在滑动的话会触发的回调
 }
